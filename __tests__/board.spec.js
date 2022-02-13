ada7b1c528045f2503a227b7d62215e6f58813b3
Bee-jay <bjpaiz@gmail.com>
2022-02-13 15:17:39 +0000

    placeship function passes test

--- __tests__/board.spec.js
+++ __tests__/board.spec.js
@@ -2,10 +2,20 @@ const createBoard = require('../factories/boards.js')
 
 describe("Gameboard factory:", () => {
   const board = createBoard();
+  board.placeShip(0, 1, 'horizontal', 2)
 
   test("instantiates 10 x 10 board object", () => {
-    expect(board.length).toBe(10)
-    expect(board[0].length.tobe(10))
+    expect(board.array.length).toBe(10)
+    expect(board.array[0].length).toBe(10)
   })
+
+  test("instantiates ship object into fleet array", () => {
+    expect(board.fleet[0].hits).toEqual([1,2])
+  }) 
+
+  test("places ship onto board", () => {
+    expect(board.array[0][1].hasShip).toBe(true)
+    expect(board.array[0][2].hasShip).toBe(true)
+  } )
 })  
 
--- factories/boards.js
+++ factories/boards.js
@@ -1,18 +1,43 @@
+const createShip = require('../factories/ships.js')
+
 const createBoard = () => {
   const board = {}
+  board.array = [];
 
-  board.array = [
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[],[]],
-  ]
+  for (i = 0; i <= 9; i++) {
+    board.array.push(new Array)
+    for (j = 0; j <= 9; j++) {
+      board.array[i].push({
+        hasShip: false,
+        isHit: false,
+        shipType: undefined,
+      })
+    }
+  }
+
+  board.fleet = [];
+
+  board.placeShip = function(y, x, direction, length) {
+    this.fleet.push(createShip(length))
+    
+    if (direction === 'horizontal') {
+      for (i = 1; i <= length; i++) {
+        for (j = x; j <= length; j++) {
+          this.array[y][j].hasShip = true;
+          this.array[y][j].isHit = false;
+        }
+      }  
+    }
+
+    if (direction === 'vertical') {
+      for (i = 1; i <= length; i++) {
+        for (j = y; j <= length; j++) {
+          this.array[y][j].hasShip = true;
+          this.array[y][j].isHit = false;
+        }
+      }  
+    }
+  }
 
   return board;
 
