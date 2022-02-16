cbf0df467c095893af1f70526e73c2f5751f3038
Bee-jay <bjpaiz@gmail.com>
2022-02-16 15:51:00 +0000

    ship object in fleet array records hit

--- factories/boards.js
+++ factories/boards.js
@@ -18,23 +18,23 @@ const createBoard = () => {
 
   board.fleet = [];
 
-  board.placeShip = function(y, x, direction, length) {
-    this.fleet.push(createShip(length))
+  board.placeShip = function(y, x, direction, length, type) {
+    this.fleet.push(createShip(length, type))
     
     if (direction === 'horizontal') {
-      for (i = 1; i <= length; i++) {
-        for (j = x; j <= length; j++) {
+      for (i = 1; i < length; i++) {
+        for (j = x; j < x + length; j++) {
           this.array[y][j].hasShip = true;
-          this.array[y][j].isHit = false;
+          this.array[y][j].shipType = type;
         }
       }  
     }
 
     if (direction === 'vertical') {
-      for (i = 1; i <= length; i++) {
-        for (j = y; j <= length; j++) {
+      for (i = 1; i < length; i++) {
+        for (j = y; j < y + length; j++) {
           this.array[j][x].hasShip = true;
-          this.array[j][x].isHit = false;
+          this.array[j][x].shipType = type;
         }
       }  
     }
@@ -42,11 +42,18 @@ const createBoard = () => {
 
   board.receiveAttack = function(y, x) {
     if (this.array[y][x].hasShip == true) {
+      let type = this.array[y][x].shipType;
+      let index = this.findShip(type);
+      this.fleet[index].hits.pop();
       this.array[y][x].isHit = true;
     } else {
       this.array[y][x].isMissed = true;
     }
   };
+
+  board.findShip = function(type) {
+    return this.fleet.indexOf(this.fleet.find((ship) => ship.type == type))
+  }
 
   return board;
 
